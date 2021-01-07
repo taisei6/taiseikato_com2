a92d79296237e779c2d120018f31af6b7ad1da4d
taisei kato <59239865+taisei6@users.noreply.github.com>
2021-01-07 17:04:08 +0900

    スライダーバグ修正

--- works/gomi.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('#slide_0 .bxslider').bxSlider();
-    $('#slide_1 .bxslider').bxSlider();
-    $('#slide_2 .bxslider').bxSlider({
-        pagerCustom: '#bx-pager'
-    });
-});
