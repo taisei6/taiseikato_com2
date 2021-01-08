7e817e0888c92119e36d819c7e66495bf7f3a546
taisei kato <59239865+taisei6@users.noreply.github.com>
2021-01-09 00:26:56 +0900

    完成間近，フォルダ整理パス変更

---
+++ js/slide.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('#slide_1 .bxslider').bxSlider({
+      auto: true,
+      pause: 4000,
+    });
+    $('#slide_2 .bxslider').bxSlider({
+      auto: true,
+      pause: 4000,
+    });
+    $('#slide_3 .bxslider').bxSlider({
+      auto: true,
+      pause: 4000,
+    });
+    $('#slide_4 .bxslider').bxSlider({
+      auto: true,
+      pause: 4000,
+    });
+});
