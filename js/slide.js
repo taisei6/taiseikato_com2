349e7ee590c78367a07baea2d4eca5a777cbb737
taisei kato <59239865+taisei6@users.noreply.github.com>
2022-10-12 21:53:52 +0900

    スライド時間追加

--- js/slide.js
+++ js/slide.js
@@ -1,18 +1,18 @@
 $(function () {
     $('#slide_1 .bxslider').bxSlider({
       auto: true,
-      pause: 4000,
+      pause: 6000,
     });
     $('#slide_2 .bxslider').bxSlider({
       auto: true,
-      pause: 4000,
+      pause: 6000,
     });
     $('#slide_3 .bxslider').bxSlider({
       auto: true,
-      pause: 4000,
+      pause: 6000,
     });
     $('#slide_4 .bxslider').bxSlider({
       auto: true,
-      pause: 4000,
+      pause: 6000,
     });
 });
